e9526363343a4668ac0521500b62755c3c62bab4
itsahmad74 <altafahmad@sartiaglobal.com>
2023-07-07 18:02:41 +0530

    update

--- webpack/rules/css.js
+++ webpack/rules/css.js
@@ -10,15 +10,12 @@
  * + @Exporting Module
  */
 
-
 // ---------------------
 // @Loading Dependencies
 // ---------------------
 
-const
-  manifest          = require('../manifest'),
-  ExtractTextPlugin = require('mini-css-extract-plugin');
-
+const manifest = require("../manifest"),
+  ExtractTextPlugin = require("mini-css-extract-plugin");
 
 // ---------------
 // @Common Loaders
@@ -28,14 +25,13 @@ let rule = {};
 
 const loaders = [
   {
-    loader: 'css-loader',
+    loader: "css-loader",
     options: {
-      sourceMap : manifest.IS_DEVELOPMENT,
+      sourceMap: manifest.IS_DEVELOPMENT,
       importLoaders: 1,
     },
   },
 ];
-
 
 // ---------------------------
 // @Merging Production Loaders
@@ -44,12 +40,13 @@ const loaders = [
 if (manifest.IS_PRODUCTION) {
   rule = {
     test: /\.css$/,
-    use: [{
+    use: [
+      {
         loader: ExtractTextPlugin.loader,
-    }].concat(loaders),
+      },
+    ].concat(loaders),
   };
 }
-
 
 // ----------------------------
 // @Merging Development Loaders
@@ -58,12 +55,13 @@ if (manifest.IS_PRODUCTION) {
 if (manifest.IS_DEVELOPMENT) {
   rule = {
     test: /\.css$/,
-    use: [{
-      loader: 'style-loader',
-    }].concat(loaders),
+    use: [
+      {
+        loader: "style-loader",
+      },
+    ].concat(loaders),
   };
 }
-
 
 // -----------------
 // @Exporting Module
